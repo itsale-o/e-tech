7d26fc036d64d52b7b1f1607f6ed56bdbaf84a3d
Alessandra Oliveira <alessandra_santos_oliveira@hotmail.com>
2024-06-23 18:21:55 -0300

    configs no app

--- curso/apps.py
+++ curso/apps.py
@@ -4,3 +4,14 @@ from django.apps import AppConfig
 class CursoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'curso'
+
+    def ready(self):
+        from .models import Usuario
+        import os
+
+        email = os.getenv("EMAIL_ADMIN")
+        senha = os.getenv("SENHA_ADMIN")
+
+        usuarios = Usuario.objects.filter(email=email)
+        if not usuarios:
+            Usuario.objects.create_superuser(username='admin', email=email, password=senha, is_active=True, is_staff=True)
