10f7960ad240c79a7de79150c35d804afccc5a1b
Alessandra Oliveira <alessandra_santos_oliveira@hotmail.com>
2024-06-23 17:45:15 -0300

    mudança no bd

--- etech/settings.py
+++ etech/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
 import dj_database_url
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = 'postgresql://postgres:ORhlHOzTAuEyCMblPGSNqYrPznwExOnk@roundhouse.proxy.rlwy.net:45489/railway'
 if DATABASE_URL:
     DATABASES = {
         'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
